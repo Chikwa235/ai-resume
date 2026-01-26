42c1d30704cdcb0a1451ac9092dc2fbdd6c435b1
Chikwa235 <chisapachikwa25@gmail.com>
2026-01-26 23:25:59 +0200

    feat: create puter.ts and puter.d.ts

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -15,22 +15,24 @@ export default function Home() {
       <main className="bg-[url('bg-main.svg')] bg-cover">
         <Navbar />
 
+
         <section className="main-section">
-          <div className="page-heading">
+          <div className="page-heading py-16">
             <h1>Track Your Applications & Resume Ratings</h1>
             <h2>Review your submissions and check AI-powered feedback.</h2>
           </div>
-        </section>
+
           {resumes.length > 0 && (
 
 
-        <div className="resumes-section flex flex-col items-center gap-6 ">
+        <div className="resumes-section flex flex-row flex-wrap justify-center gap-6 ">
             {resumes.map((resume) => (
                 <ResumeCard key={resume.id} resume={resume} />
             ))}
         </div>
 
           )}
+        </section>
       </main>
   );
 }
